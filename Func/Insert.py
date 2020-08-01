45d53cf969f2aab7539eb4a087eef8a4f512afe5
Ksenia Lebedeva <ksenyalebedi99@mail.ru>
2020-08-01 14:02:47 +0500

    Update Insert.py

--- Func/Insert.py
+++ Func/Insert.py
@@ -14,17 +14,42 @@ class InsertDialog(QDialog):
         self.setFixedHeight(250)
         self.QBtn.clicked.connect(self.addWork)
         layout = QVBoxLayout()
+        
+        self.nameStr=QLabel()
+        self.nameStr.setText("Название вакансии:")
         self.nameinput = QLineEdit()
         self.nameinput.setPlaceholderText("Название")
+        layout.addWidget(self.nameStr)
         layout.addWidget(self.nameinput)
+
+        self.nameStr=QLabel()
+        self.nameStr.setText("Номер страницы:")
+        self.seminput = QComboBox()
+        self.seminput.addItem("0")
+        self.seminput.addItem("1")
+        self.seminput.addItem("2")
+        self.seminput.addItem("3")
+        self.seminput.addItem("4")
+        self.seminput.addItem("5")
+        self.seminput.addItem("6")
+        self.seminput.addItem("7")
+        self.seminput.addItem("8")
+        self.seminput.addItem("9")
+        self.seminput.addItem("10")
+        layout.addWidget(self.nameStr)
+        layout.addWidget(self.seminput)
+
+        self.nameStr = QLabel()
+        self.nameStr.setText("Область:")
         self.branchinput = QComboBox()
-        self.branchinput.addItem("Архангельская область")
         self.branchinput.addItem("Свердловкая область")
         self.branchinput.addItem("Московская область")
         self.branchinput.addItem("Ростовская область")
         self.branchinput.addItem("Курская область")
         self.branchinput.addItem("Новгородская область")
+        layout.addWidget(self.nameStr)
         layout.addWidget(self.branchinput)
+
         layout.addWidget(self.QBtn)
         self.setLayout(layout)
 
@@ -33,13 +58,14 @@ class InsertDialog(QDialog):
                   'Новгородская область':1051,'Ростовская область':1530}
         k=listArea.keys()
         name=self.nameinput.text()
+        sem = self.seminput.itemText(self.seminput.currentIndex())
         branch = self.branchinput.itemText(self.branchinput.currentIndex())
         for el in k:
             if el==branch:
                 i=listArea[el]
         url = 'https://api.hh.ru/vacancies/'
         par = {'text': name,
-               'premium': 'false', 'area':i, 'per_page': '100', 'page': [el for el in range(0, 200)]}
+               'premium': 'false', 'area':i, 'per_page': '100', 'page': sem}
         for i in requests.get(url, params=par).json()['items']:
             key_skills_string = ''
             vac_id = i['id']
